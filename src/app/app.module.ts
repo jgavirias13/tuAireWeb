8b54729c22e3943b48e66c1b4fc70261dc5197b4
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2019-04-07 22:16:16 -0500

    Primer diseño con elementos basicos, obtener coordenadas del usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card'; 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PmAireComponent } from './pm-aire/pm-aire.component';
@@ -16,7 +14,6 @@ import { PmAireComponent } from './pm-aire/pm-aire.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pm-aire/pm-aire.component.ts
+++ src/app/pm-aire/pm-aire.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationService } from '../location.service';
 
 @Component({
   selector: 'app-pm-aire',
   templateUrl: './pm-aire.component.html',
-  styleUrls: ['./pm-aire.component.sass']
+  styleUrls: ['./pm-aire.component.scss']
 })
 export class PmAireComponent implements OnInit {
 
-  constructor() { }
+  ubicacion: {};
+
+  constructor(private ls: LocationService) { }
 
   ngOnInit() {
+    this.ls.getPosition().then(pos =>{
+      console.log(`Positon: ${pos.lng} ${pos.lat}`);
+      this.ubicacion = {
+        longitud: pos.lng,
+        latitud: pos.lat
+      };
+    })
   }
 
 }
